1020e6a4f26629d8bd1a5afc0da82b6b2836140c
Etsku520 <eetu.huovila@gmail.com>
2018-04-02 17:46:03 +0300

    ...

--- application/auth/views.py
+++ application/auth/views.py
@@ -21,7 +21,7 @@ def auth_login():
         return render_template("auth/loginform.html", form = form,
                                error = "No such username or password")
     
-    if not user.check_password(form.password.data.encode('utf-8')):
+    if not user.check_password(form.password.data):
         return render_template("auth/loginform.html", form = form,
                                error = "No such username or password")
 
@@ -51,7 +51,7 @@ def auth_register():
                                form = form)
 
     u = User(form.name.data, form.username.data,
-             bcrypt.generate_password_hash(form.password.data.encode('utf-8')))
+             bcrypt.generate_password_hash(form.password.data))
 
     if does_exist_name(u):
         error = "name is laready taken"
